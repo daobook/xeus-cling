97bc4a4f114129cf9f54b9d5b2cf98e74a2eccf0
xinetzone <xinzone@outlook.com>
2021-12-09 20:56:13 +0800

    zh_CN

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -16,7 +16,7 @@ html_theme = "sphinx_rtd_theme"
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 def setup(app):
-    app.add_stylesheet("main_stylesheet.css")
+    app.add_css_file("main_stylesheet.css")
 
 extensions = ['breathe']
 breathe_projects = { 'xeus-cling': '../xml' }
@@ -35,3 +35,7 @@ highlight_language = 'c++'
 pygments_style = 'sphinx'
 todo_include_todos = False
 htmlhelp_basename = 'xeus-clingdoc'
+
+language = 'zh_CN'
+locale_dirs = ['../locales/']  # path is example but recommended.
+gettext_compact = False  # optional.
